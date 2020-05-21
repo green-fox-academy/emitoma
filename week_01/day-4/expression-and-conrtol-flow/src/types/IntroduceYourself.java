d4c5e4357add04449f009c6e635407a164f6b222
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-21 16:58:38 +0200

    Finished Types exercises

---
+++ week_01/day-4/expression-and-conrtol-flow/src/types/IntroduceYourself.java
@@ -0,0 +1,13 @@
+package types;
+
+public class IntroduceYourself {
+    public static void  main (String[] args) {
+        String myName = "Emi";
+        byte myAge = 24;
+        float myHeigth = 1.71F;
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeigth);
+    }
+}
---
+++ week_01/day-4/expression-and-conrtol-flow/src/types/TwoNumbers.java
@@ -0,0 +1,22 @@
+package types;
+
+public class TwoNumbers {
+    public static void  main (String [] args){
+        byte num1 = 22;
+        byte num2 = 13;
+
+        // Print the result of 13 added to 22
+        System.out.println(num1 + num2);
+        // Print the result of 13 substracted from 22
+        System.out.println(num1 - num2);
+        // Print the result of 22 multiplied by 13
+        System.out.println(num1 * num2);
+        // Print the result of 22 divided by 13 (as a decimal fraction)
+        System.out.println((double) num1 / num2);
+        // Print the integer part of 22 divided by 13
+        System.out.println(num1 / num2);
+        // Print the remainder of 22 divided by 13
+        System.out.println(num1 % num2);
+
+    }
+}
