9267ff588f2c1e63761e65e43f49f599c4c32499
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-21 15:43:58 +0200

    Finished printing to the terminal window exercises.

---
+++ week_01/day-4/expression-and-conrtol-flow/src/hello_me/HelloME.java
@@ -0,0 +1,7 @@
+package hello_me;
+
+public class HelloME {
+    public static void main (String[] args){
+        System.out.println("Hello, Emi!");
+    }
+}
---
+++ week_01/day-4/expression-and-conrtol-flow/src/hello_others/HelloOthers.java
@@ -0,0 +1,9 @@
+package hello_others;
+
+public class HelloOthers {
+    public static void main(String[] args){
+        System.out.println("Hello, Szid");
+        System.out.println("Hello, Bence");
+        System.out.println("Hello, Eszter");
+    }
+}
