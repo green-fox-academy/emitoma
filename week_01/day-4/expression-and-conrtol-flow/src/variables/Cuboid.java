c4da66aadd7fc1c298899bd2adc69e87a5465f71
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-21 22:36:46 +0200

    Started Cuboid, unfinished.

---
+++ week_01/day-4/expression-and-conrtol-flow/src/variables/Cuboid.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class Cuboid {
+    public static void main(String[] args){
+
+        // Write a program that stores 3 sides of a cuboid as variables (doubles)
+        // The program should write the surface area and volume of the cuboid like:
+        //
+        // Surface Area: 600 - > 2*(a*b + a*c + b*c)
+        // Volume: 1000 - > a * b * c
+
+        double a = 0;
+        double b = 0;
+        double c = 0;
+
+        double surfaceArea = 600;
+        double volume = 1000;
+
+    }
+}
