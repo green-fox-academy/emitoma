1d2f119bcbae7bc42350c9ba55ca093097801929
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-22 15:25:11 +0200

    Added DrawTriangle

---
+++ week_01/day-4/expression-and-conrtol-flow/src/loops/DrawTriangle.java
@@ -0,0 +1,23 @@
+package loops;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please write a number");
+        int myNum = scanner.nextInt();
+
+        for (int i = 0; i <= myNum; i++) {
+            for (int j = 0; j <= myNum; j++) {
+                if (j < i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
