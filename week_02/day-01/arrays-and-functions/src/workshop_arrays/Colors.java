fa955a709080a021ba98a945b7974b259fb586e7
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-25 14:49:51 +0200

    Added Colors.java.

---
+++ week_02/day-01/arrays-and-functions/src/workshop_arrays/Colors.java
@@ -0,0 +1,11 @@
+package workshop_arrays;
+
+public class Colors {
+    public static void main(String[] args) {
+
+        String[][] colorsArray = {{"lime", "forest green", "olive", "pale green", "spring green"}
+        ,{"orange red", "red", "tomato"},{"orchid", "violet", "pink", "hot pink"}};
+        
+
+    }
+}
