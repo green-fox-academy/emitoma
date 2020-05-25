629d700dcbb3fc267a7ce61d6078c7a43d984e5f
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-25 14:02:22 +0200

    Adde change and sum elements.

---
+++ week_02/day-01/arrays-and-functions/src/workshop_arrays/ChangeElement.java
@@ -0,0 +1,14 @@
+package workshop_arrays;
+
+public class ChangeElement {
+    public static void main(String[] args) {
+// - Create an array variable named `numbers`
+//   with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4
+// - Print the fourth element
+        int numbers[] = {1,2,3,8,5,6};
+        numbers[3] = 4;
+        System.out.println(numbers[3]);
+
+    }
+}
