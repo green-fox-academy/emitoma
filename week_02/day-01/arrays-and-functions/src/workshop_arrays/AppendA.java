6d70b963c3d4ac8888169926e3908ccc05789d25
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-25 15:05:49 +0200

    added AppendA, SwapElements.java

---
+++ week_02/day-01/arrays-and-functions/src/workshop_arrays/AppendA.java
@@ -0,0 +1,16 @@
+package workshop_arrays;
+
+public class AppendA {
+    public static void main(String[] args) {
+        // - Create an array variable named `animals`
+        //   with the following content: `["koal", "pand", "zebr"]`
+        // - Add all elements an `"a"` at the end
+        String[] animals = {"koal", "pand", "zebr"};
+        for (int i = 0; i < animals.length; i++) {
+            String append = animals[i] + "a";
+            System.out.println(append);
+        }
+
+
+    }
+}
--- week_02/day-01/arrays-and-functions/src/workshop_arrays/Colors.java
+++ week_02/day-01/arrays-and-functions/src/workshop_arrays/Colors.java
@@ -5,7 +5,7 @@ public class Colors {
 
         String[][] colorsArray = {{"lime", "forest green", "olive", "pale green", "spring green"}
         ,{"orange red", "red", "tomato"},{"orchid", "violet", "pink", "hot pink"}};
-        
+
 
     }
 }
