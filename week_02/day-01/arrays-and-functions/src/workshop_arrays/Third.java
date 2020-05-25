8e130fcd02a5a818dac107d62caa3db1e0121159
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-25 13:55:46 +0200

    Added compareLength.java and third.java

---
+++ week_02/day-01/arrays-and-functions/src/workshop_arrays/Third.java
@@ -0,0 +1,10 @@
+package workshop_arrays;
+
+public class Third {
+
+    public static void main(String[] args) {
+        int numbers[] = {4, 5, 6, 7};
+        System.out.println(numbers[2]);
+    }
+
+}
