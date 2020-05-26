82c22ce7fb5b47156f67b8ee1e9997fd6e5eb799
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-26 12:59:27 +0200

    Added SubInt.java and Unique.java, modified .gitignore, added  SimpleReplace.java

---
+++ week_02/day-01/arrays-and-functions/src/func/Unique.java
@@ -0,0 +1,15 @@
+package func;
+
+import java.util.Arrays;
+
+public class Unique {
+    public static void main(String[] args) {
+        int[] myArr = {1, 11, 34, 11, 52, 61, 1, 34};
+
+    }
+
+    public static int[] unique(int n, int[] arr) {
+
+    }
+
+}
---
+++ week_02/day-02/src/strings/SimpleReplace.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class SimpleReplace {
+    public static void main(String[] args) {
+        String example = "In a dishwasher far far away";
+
+
+        // I would li
+        // ke to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+        // Please fix it for me!
+        // Expected output: In a galaxy far far away
+
+        System.out.println(example.replace("dishwasher", "galaxy"));
+        System.out.println(example);
+    }
+}
