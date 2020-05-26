05d5fb478995a180006546fc6b4f3e6d4b47b38a
emitoma <toma.emi.95.07.17@gmail.com>
2020-05-27 00:15:27 +0200

    Added TodoPrint.java

---
+++ week_02/day-02/src/strings/TodoPrint.java
@@ -0,0 +1,24 @@
+package strings;
+
+public class TodoPrint {
+    public static void main(String[] args) {
+        String todoText = " - Buy milk\n";
+        // Add "My todo:" to the beginning of the todoText
+        // Add " - Download games" to the end of the todoText
+        // Add " - Diablo" to the end of the todoText but with indention
+
+        // Expected output:
+
+        // My todo:
+        //  - Buy milk
+        //  - Download games
+        //      - Diablo
+        todoText = "My todo\n".concat(todoText);
+        todoText = todoText.concat(" - Download games\n");
+        todoText = todoText.concat("\t - Diablo");
+
+
+        System.out.println(todoText);
+    }
+}
+
