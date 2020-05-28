32e757ff8089bd02e98d1d9000242e7b221fc509
Toma Emilia <toma.emi.95.07.17@gmail.com>
2020-05-28 11:54:20 +0200

    Added LineInTheMiddle

--- week_02/day-04-05/src/drawing/LineInTheMiddle.java
+++ week_02/day-04-05/src/drawing/LineInTheMiddle.java
@@ -1,4 +1,50 @@
 package drawing;
 
+import javax.swing.*;
+
+import java.awt.*;
+
+import static java.awt.Window.getOwnerlessWindows;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 public class LineInTheMiddle {
+
+
+    public static void mainDraw(Graphics graphics) {
+        // draw a red horizontal line to the canvas' middle.
+        // draw a green vertical line to the canvas' middle.
+        int xMiddle = WIDTH / 2;
+        int yMiddle = HEIGHT / 2;
+        graphics.setColor(Color.RED);
+        graphics.drawLine(0, yMiddle, WIDTH, yMiddle);
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(xMiddle, 0, xMiddle, HEIGHT);
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
 }
+
+
