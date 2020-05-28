c3255e75a936e3b5a05b3e23f42f46db67d6f3a2
Toma Emilia <toma.emi.95.07.17@gmail.com>
2020-05-28 18:09:19 +0200

    Added CenteredSquare, FourRect,Rainbowbox

---
+++ week_02/day-04-05/src/drawing/FourRectangles.java
@@ -0,0 +1,67 @@
+package drawing;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Random;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+
+    public static void mainDraw(Graphics graphics) {
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+        for (int i = 0; i < 4; i++) {
+            double x = i * 20;
+            double y = (i * 20) + 10;
+            int width = 100 + 10 * i;
+            int height = width;
+            drawRectangle((int) x, (int) y, width, height, graphics);
+        }
+
+
+    }
+
+    public static void drawRectangle(int x, int y, int width, int height, Graphics graphics) {
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            int r = random.nextInt(255);
+            int g = random.nextInt(255);
+            int b = random.nextInt(255);
+
+            Graphics2D g2 = (Graphics2D) graphics;
+            g2.setStroke(new BasicStroke(3));
+
+            graphics.setColor(new Color(r, g, b));
+            graphics.fillRect(x, y, width, height);
+
+
+        }
+    }
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("FourRectangles");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
+
---
+++ week_02/day-04-05/src/drawing/RainbowBoxFunction.java
@@ -0,0 +1,67 @@
+package drawing;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class RainbowBoxFunction {
+    public static void mainDraw(Graphics graphics) {
+        // Create a square drawing function that takes 3 parameters:
+        // The square size, the fill color, graphics
+        // and draws a square of that size and color to the center of the canvas.
+        // Create a loop that fills the canvas with rainbow colored squares (red, orange, yellow, green, blue, indigo, violet).
+        ArrayList<Color> rainbow = new ArrayList<>();
+        rainbow.add(Color.RED);
+        rainbow.add(Color.ORANGE);
+        rainbow.add(Color.YELLOW);
+        rainbow.add(Color.GREEN);
+        rainbow.add(Color.BLUE);
+        rainbow.add(new Color(75, 0, 130));
+        rainbow.add(new Color(238, 130, 238));
+
+        for (int i = 0; i < rainbow.size(); i++) {
+            int size = 20 + (i * 10);
+            Color myColor = rainbow.get(i);
+
+
+            squareDrawer(size, myColor, graphics);
+        }
+    }
+
+    public static void squareDrawer(int size, Color fillColor, Graphics g) {
+
+        int xCenter = (WIDTH / 2) - (size / 2); // coordinates of the top left corner of the square
+        int yCenter = (HEIGHT / 2) - (size / 2); // coordinates of the top left corner of the square
+
+        g.setColor(fillColor);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(20));
+        g.drawRect(xCenter, yCenter, size, size);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("RainbowBow");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
