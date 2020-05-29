ce3e375c4286e17f15669fce1973545386474330
Toma Emilia <toma.emi.95.07.17@gmail.com>
2020-05-29 18:29:52 +0200

    Added some comments

--- week_01/day-4/expression-and-conrtol-flow/src/loops/GuessTheNumber.java
+++ week_01/day-4/expression-and-conrtol-flow/src/loops/GuessTheNumber.java
@@ -6,26 +6,30 @@ import java.util.Scanner;
 public class GuessTheNumber {
     public static void main(String[] args) {
 
-        // Write a program that stores a number, and the user has to figure it out.
-        // The user can input guesses, after each guess the program would tell one
-        // of the following:
-        //
-        // The stored number is higher
-        // The stried number is lower
-        // You found the number: 8
+//         Write a program that stores a number, and the user has to figure it out.
+//         The user can input guesses, after each guess the program would tell one
+//         of the following:
+//
+//         The stored number is higher
+//         The stried number is lower
+//         You found the number: 8
+
+        // Make a scanner to read the user input
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please write a number between 0 and 25");
+
+        //Make a random number
 
 //      int storedNum = (int) (Math.random() * 10);
         Random random = new Random();
         int storedNum = random.nextInt(25);
 
-        System.out.println(storedNum);
 
+        //Switch for the game, is false the game goes on, if true the game ends.
         boolean foundTheNumber = false;
 
-
         do {
+            // Read the user input
             int userGuess = scanner.nextInt();
             if (userGuess == storedNum) {
                 System.out.println("You found the number: " + storedNum);
