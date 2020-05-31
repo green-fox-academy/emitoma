2dc312057d540f5e0985be8bc0f3d583a51b07c9
Toma Emilia <toma.emi.95.07.17@gmail.com>
2020-05-31 15:12:06 +0200

    Finished unique, deleted SubInt

--- week_02/day-01/arrays-and-functions/src/func/Unique.java
+++ week_02/day-01/arrays-and-functions/src/func/Unique.java
@@ -1,15 +1,27 @@
 package func;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Unique {
     public static void main(String[] args) {
+        //  Create a function that takes a list of numbers as a parameter
+        //  Returns a list of numbers where every number in the list occurs only once
+
+        //  Example
+//        System.out.println(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
+        //  should print: `[1, 11, 34, 52, 61]`
         int[] myArr = {1, 11, 34, 11, 52, 61, 1, 34};
+        System.out.println(Arrays.toString(unique(myArr).toArray()));
 
     }
 
-    public static int[] unique(int n, int[] arr) {
+    public static TreeSet<Integer> unique(int[] arr) {
+        TreeSet<Integer> unique = new TreeSet<>();
 
+        for (int i = 0; i < arr.length; i++) {
+            unique.add(arr[i]);
+        }
+
+        return unique;
     }
-
 }
