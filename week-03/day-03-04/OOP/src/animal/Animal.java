201a96154a30ec00c8458b63378ff831071cf732
Toma Emilia <toma.emi.95.07.17@gmail.com>
2020-06-03 15:53:33 +0200

    Added Animal

--- week-03/day-03-04/OOP/src/animal/Animal.java
+++ week-03/day-03-04/OOP/src/animal/Animal.java
@@ -1,6 +1,28 @@
 package animal;
 
 public class Animal {
-    private int hunger;
-    private 
+    private int hunger = 0;
+    private int thirst = 0;
+
+    public Animal() {
+        this.hunger = 50;
+        this.thirst = 50;
+    }
+
+    public void eat() {
+        this.hunger--;
+        System.out.println("Hunger level is: " + hunger);
+    }
+
+    public void drink() {
+        this.thirst--;
+        System.out.println("Thirst level is: " + thirst);
+    }
+
+    public void play() {
+        this.hunger++;
+        this.thirst++;
+        System.out.println("After playing, hunger level is: " + hunger + " and thirst level is:" + thirst);
+    }
+
 }
--- week-03/day-03-04/OOP/src/animal/Main.java
+++ week-03/day-03-04/OOP/src/animal/Main.java
@@ -2,6 +2,10 @@ package animal;
 
 public class Main {
     public static void main(String[] args) {
+        Animal cat = new Animal();
+        cat.play();
+        cat.drink();
+        cat.eat();
 
 
     }
