74a019cecafb2a950f1acf60f333b019836230ef
Toma Emilia <toma.emi.95.07.17@gmail.com>
2020-06-09 16:32:38 +0200

    Add Flyable

---
+++ week_04/day-02/src/flyable/FlyableInterface.java
@@ -0,0 +1,10 @@
+package flyable;
+
+public interface FlyableInterface {
+    public void land();
+
+    public void fly();
+
+    public void takeOff();
+
+}
---
+++ week_04/day-02/src/flyable/Helicopter.java
@@ -0,0 +1,19 @@
+package flyable;
+
+public class Helicopter extends Vehicle implements FlyableInterface{
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+}
