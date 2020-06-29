9de12205da08e3fa937d872b44158bb378805dd4
Toma Emilia <toma.emi.95.07.17@gmail.com>
2020-06-29 22:27:58 +0200

    Finished excercise 6

--- week_06/day-01/src/exercises/ExerciseSix.java
+++ week_06/day-01/src/exercises/ExerciseSix.java
@@ -1,7 +1,21 @@
 package exercises;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class ExerciseSix {
     public static void main(String[] args) {
+//        Write a Stream Expression to find the uppercase characters in a string!
+        String mysString = "AbCdE";
+        String result = mysString
+                .chars()
+                .filter(Character::isUpperCase)
+                .mapToObj(c -> Character.toString((char) c))
+                .collect(Collectors.joining());
+
+        System.out.println(result);
 
     }
 }
