248ce1e3805c5a77c550647b95d3630c4db9c7ab
Toma Emilia <toma.emi.95.07.17@gmail.com>
2020-06-29 16:43:08 +0200

    Add E 6

--- week_06/day-01/src/exercises/ExerciseFive.java
+++ week_06/day-01/src/exercises/ExerciseFive.java
@@ -8,5 +8,7 @@ public class ExerciseFive {
 //        Write a Stream Expression to get the sum of the odd numbers in the following list:
 
         List<Integer> numbers = Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2);
+        double result = numbers.stream().filter(n -> n % 2 != 0).mapToInt(n -> n).sum();
+        System.out.println(result);
     }
 }
---
+++ week_06/day-01/src/exercises/ExerciseSix.java
@@ -0,0 +1,7 @@
+package exercises;
+
+public class ExerciseSix {
+    public static void main(String[] args) {
+
+    }
+}
