317170be3a574489937d8eb8f17cd65a4651d324
Toma Emilia <toma.emi.95.07.17@gmail.com>
2020-07-02 20:24:47 +0200

    Finish extending tasks, prettify,raise balance and create new account

--- week_06/day-04/src/main/java/com/example/bankofsimba/controller/BankAccountController.java
+++ week_06/day-04/src/main/java/com/example/bankofsimba/controller/BankAccountController.java
@@ -3,33 +3,31 @@ package com.example.bankofsimba.controller;
 import com.example.bankofsimba.model.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.lang.invoke.SwitchPoint;
+import java.util.*;
 
 @Controller
 public class BankAccountController {
 
     List<BankAccount> accounts = new ArrayList<>(Arrays.asList(
-            new BankAccount("Zordon", 10000, "lion"),
-            new BankAccount("Zazu", 3000, "hornbill"),
-            new BankAccount("Timon", 500, "meerkat"),
-            new BankAccount("Rafiki", 20000, "mandrill")
+            new BankAccount("Scar", 10000, "lion", false, true),
+            new BankAccount("Zazu", 3000, "hornbill", false, false),
+            new BankAccount("Timon", 500, "meerkat", false, false),
+            new BankAccount("Rafiki", 20000, "mandrill", false, false),
+            new BankAccount("Simba", 2000, "lion", true, false)
     ));
     BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
 
 
     @GetMapping("/show")
     public String show(Model model) {
-
         model.addAttribute("bankaccount", bankAccount);
         return "index";
     }
 
-    @GetMapping("/enjoy")
+    @GetMapping("/html")
 //    @ResponseBody
     public String enjoy(Model model) {
         String enjoyString = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
@@ -37,12 +35,38 @@ public class BankAccountController {
         return "enjoy";
     }
 
-    @GetMapping("/allaccount")
-    public String allacc(Model model) {
+    @GetMapping("/account")
+    public String allAccounts(Model model) {
 
         model.addAttribute("accounts", accounts);
 
-
         return "accounts";
     }
+
+    @PostMapping("/save-account")
+    public String saveAcc(@ModelAttribute BankAccount bankAccount) {
+        accounts.add(bankAccount);
+
+        return "redirect:/account";
+    }
+
+    @GetMapping("/increase-balance")
+    public String increaseBalance(@RequestParam String name, Model model) {
+        Optional<BankAccount> optionalBankAccount = findAccount(name);
+
+        if (optionalBankAccount.isPresent()) {
+            BankAccount bankAccount = optionalBankAccount.get();
+            bankAccount.incrementBalance();
+
+        }
+
+        return "redirect:/account";
+    }
+
+    private Optional<BankAccount> findAccount(String name) {
+        return accounts.stream()
+                .filter(a -> a.getName().equals(name))
+                .findFirst();
+    }
+
 }
--- week_06/day-04/src/main/java/com/example/bankofsimba/model/BankAccount.java
+++ week_06/day-04/src/main/java/com/example/bankofsimba/model/BankAccount.java
@@ -4,11 +4,24 @@ public class BankAccount {
     private String name;
     private int balance;
     private String animalType;
+    private boolean isKing;
+    private boolean isBadGuy;
+
+    public BankAccount() {
+    }
 
     public BankAccount(String name, int balance, String animalType) {
         this.name = name;
         this.balance = balance;
         this.animalType = animalType;
+    }
+
+    public BankAccount(String name, int balance, String animalType, boolean isKing, boolean isBadGuy) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+        this.isKing = isKing;
+        this.isBadGuy = isBadGuy;
     }
 
     public String getName() {
@@ -33,5 +46,37 @@ public class BankAccount {
 
     public void setAnimalType(String animalType) {
         this.animalType = animalType;
+    }
+
+    public boolean isKing() {
+        return isKing;
+    }
+
+    public void setIsKing(boolean isKing) {
+        this.isKing = isKing;
+    }
+
+    public boolean isBadGuy() {
+        return isBadGuy;
+    }
+
+    public void setIsBadGuy(boolean badGuy) {
+        this.isBadGuy = badGuy;
+    }
+
+    public String getKingStyleClass() {
+        return this.isKing ? "king" : "";
+    }
+
+    public String getBadString() {
+        return this.isBadGuy ? "Bad guy" : " Good one";
+    }
+
+    public void incrementBalance() {
+        if (isKing) {
+            balance += 100;
+        } else {
+            balance += 10;
+        }
     }
 }
